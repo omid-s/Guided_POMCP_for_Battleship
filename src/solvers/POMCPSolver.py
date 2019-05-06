d2c90e79f095f00da5063cb126dbc9b55d69a1e4
parya-j <parya.jandaghi@gmail.com>
2019-05-06 17:43:07 -0400

    adds initial belief calculation by sampling

--- src/solvers/POMCPSolver.py
+++ src/solvers/POMCPSolver.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 import random
 from src.battleship_board.gameboard import BattleShip
 from src.solvers.Solver import  Solver
@@ -15,7 +17,9 @@ class POMCPSolver(Solver):
         self.board_to_solve = board
         self.board_size = board.boardSize
         self.moves = 0
-        self.time_to_solve=0
+        self.time_to_solve = 0
+        self.initial_beliefs = None
+        self.init_sampelingConst = 50000
 
     def solve(self):
         """
@@ -30,4 +34,31 @@ class POMCPSolver(Solver):
     def stats(self):
         return self.moves, self.time_to_solve
 
-    
+
+    def generate_initial_beliefs_by_sampling(self):
+        """
+        if belief file is present  it will read the file as the initial beleifs
+        other wise the system will generate the beliefs
+
+        :return:
+        """
+        if os.path.isfile("beleifs.npy"):
+            self.initial_beliefs = np.load("beleifs.npy")
+            return
+
+        freq_meter = np.zeros((self.board_to_solve.boardSize, self.board_to_solve.boardSize))
+        ship_sizes  = []
+        for ship in self.board_to_solve.ships:
+            ship_sizes.append(ship.length)
+
+        for i in range(self.init_sampelingConst):
+            bt = BattleShip(self.board_to_solve.boardSize,"")
+            bt.generate_random(ship_sizes)
+            bt.board[bt.board>0] = 1
+            freq_meter+= bt.board
+
+        freq_meter /= self.init_sampelingConst # convert frequency to probability
+
+        np.save("beleifs.npy",freq_meter)
+
+
--- src/test.py
+++ src/test.py
@@ -1,9 +1,14 @@
 
 from battleship_board.gameboard import *
-
+from src.solvers.POMCPSolver import POMCPSolver
 sh1 = Ship( [1,1],2,'h' )
 bt = BattleShip(10,"test")
 bt.add_ship(sh1)
 sh2 = Ship( [2,1],3,'v' )
 bt.add_ship(sh2)
 bt.print_board()
+
+
+
+x= POMCPSolver(bt)
+x.generate_initial_beliefs_by_sampling()
